56f07df9feace8c5569fed410ac8a384ac51d64a
AdrianLopez12 <Adrianlopez20000@hotmail.com>
2022-11-29 07:21:20 -0500

    hola22

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,11 @@
 export const environment = {
-    firebase: {
-      projectId: 'compras61a',
-      appId: '1:399472214208:web:c811de44b1e35d25a96d25',
-      storageBucket: 'compras61a.appspot.com',
-      apiKey: 'AIzaSyCPwgL3pj2bMqG3rBETCmNEvqB9gk08oWw',
-      authDomain: 'compras61a.firebaseapp.com',
-      messagingSenderId: '399472214208',
-    },
-      production: true
-    };
-    
+  firebase: {
+    projectId: 'compras61a',
+    appId: '1:399472214208:web:c811de44b1e35d25a96d25',
+    storageBucket: 'compras61a.appspot.com',
+    apiKey: 'AIzaSyCPwgL3pj2bMqG3rBETCmNEvqB9gk08oWw',
+    authDomain: 'compras61a.firebaseapp.com',
+    messagingSenderId: '399472214208',
+  },
+  production: true,
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,14 +11,14 @@ export const environment = {
     authDomain: 'compras61a.firebaseapp.com',
     messagingSenderId: '399472214208',
   },
-    production: false
-  };
-  
-  /*
-   * For easier debugging in development mode, you can import the following file
-   * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
-   *
-   * This import should be commented out in production mode because it will have a negative impact
-   * on performance if an error is thrown.
-   */
-  // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+  production: false,
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
